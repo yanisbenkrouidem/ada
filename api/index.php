3ffa354d0429eb4daa7b5c5d93fb25416330b080
yanisbenkrouidem <yanis.benkrouidem@gmail.com>
2026-06-15 22:13:10 +0200

    chore: setup vercel deployment configuration (vercel.json & api/index.php)

---
+++ api/index.php
@@ -0,0 +1,5 @@
+<?php
+
+// Ce fichier sert de point d'entrée pour les Serverless Functions de Vercel.
+// Il redirige toutes les requêtes vers le framework Laravel.
+require __DIR__ . '/../public/index.php';
